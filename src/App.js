75a468875a24e06c6473a086a045de90d764193f
Larissa <larisssa_rga@hotmail.com>
2022-11-04 16:25:52 -0300

    Adiciona requisito 5

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
-import Header from './components/Header';
+// import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        {/* <Header /> */}
         <p>TrybeTunes</p>
         <div>
           <Switch>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,42 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      disabledSearch: true,
+    };
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+
+  handleSearchChange({ target }) {
+    const enabled = (target.value.length > 1);
+    this.setState({
+      disabledSearch: !enabled,
+    });
+  }
+
   render() {
+    const { disabledSearch } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <input
+            type="text"
+            name="search"
+            data-testid="search-artist-input"
+            onChange={ this.handleSearchChange }
+          />
+        </form>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ disabledSearch }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
